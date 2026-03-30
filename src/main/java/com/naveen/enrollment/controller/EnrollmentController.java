6529230126a273a87d92943834291f0cd3c246c3
sanjeev maurya <163615408+SanjeevMaurya63@users.noreply.github.com>
2026-03-30 23:55:48 +0530

    Update EnrollmentController.java

--- src/main/java/com/naveen/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/naveen/enrollment/controller/EnrollmentController.java
@@ -3,7 +3,13 @@ package com.naveen.enrollment.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.naveen.enrollment.dto.enrollment.EnrollmentRequest;
 import com.naveen.enrollment.dto.enrollment.EnrollmentSummaryResponse;
